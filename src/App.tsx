5dafc85598caab400f75f694fdfaeeea0078fd24
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-06 17:23:10 +0200

    Add tailored Moho logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import "./App.css";
-import { Typography, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import NewEvent from "./views/CreateEvent";
 import Home from "./views/LandingPage";
 import { Routes, Route, useParams, Navigate } from "react-router-dom";
 import ViewEvent from "./views/ViewEvent";
 import NoResponse from "./views/NoResponse";
+import MohoEventsLogo from "./components/MohoEventsLogo";
 
 function App() {
   const { eventID } = useParams<{ eventID: string }>();
   return (
     <>
       <Container
-        sx={{ mt: 3, mb: 3, display: "flex", flexDirection: "column" }}
+        sx={{ mt: 0.2, mb: 3, display: "flex", flexDirection: "column" }}
       >
-        <Typography color="primary.main" variant="h1">
-          MOHO EVENTS
-        </Typography>
+        <MohoEventsLogo height={80} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="creer" element={<NewEvent />} />
