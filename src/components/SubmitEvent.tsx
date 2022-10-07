9eb13bbe9a2e06a804bf44101208c5b5cfe645a9
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-07 17:55:52 +0200

    Add form validation & redirect user after form submission

--- src/components/SubmitEvent.tsx
+++ src/components/SubmitEvent.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { GroupInfo, EmptyForm } from "../typeUtils";
 import { getLocalGroups, setLocalGroups } from "../utils/manageLocalStorage";
 import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   emptyFormState: EmptyForm;
@@ -10,8 +11,34 @@ interface Props {
 }
 
 const SubmitEvent = ({ emptyFormState, formData, setFormData }: Props) => {
+  // declare hook at the component level to be called later
+  const navigate = useNavigate();
+
+  const clearOldFormData = () => {
+    setFormData({ ...emptyFormState, id: uuidv4() });
+  };
+
+  const routeUserHome = () => {
+    navigate("/");
+  };
+
+  const formNotValid = (): boolean => {
+    const valueMissing: boolean = Object.values(formData).some(
+      (value: string) => {
+        if (value === "") return true;
+        return false;
+      }
+    );
+
+    return valueMissing ? true : false;
+  };
+
   const pushEventToArray = () => {
-    console.log("pushing", formData);
+    if (formNotValid()) {
+      // todo: add error message for user
+      alert("Veuillez remplir tous les champs");
+      return;
+    }
 
     // push new group to existing groups
     const existingGroups: GroupInfo[] = getLocalGroups();
@@ -20,10 +47,7 @@ const SubmitEvent = ({ emptyFormState, formData, setFormData }: Props) => {
     // push the new inclusive array to local storage
     setLocalGroups(existingGroups);
     clearOldFormData();
-  };
-
-  const clearOldFormData = () => {
-    setFormData({ ...emptyFormState, id: uuidv4() });
+    routeUserHome();
   };
 
   return (
