3ab0a77f58544a738300a2ccf3fe0134607c7d9b
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-07 23:36:45 +0200

    Add groundwork for editing an event

--- src/views/EditEvent.tsx
+++ src/views/EditEvent.tsx
@@ -1,12 +1,22 @@
-import { Button, Box, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-
+import AllTextInputs from "../components/AllTextInputs";
+import { getLocalGroups, setLocalGroups } from "../utils/manageLocalStorage";
 import PageTitle from "../components/PageTitle";
+import { GroupInfo } from "../typeUtils";
+import { useState } from "react";
 
 const EditEvent = () => {
+  const [formData, setFormData] = useState<GroupInfo>({} as GroupInfo);
+
+  const localGroups = getLocalGroups();
+
+  const currentGroup: GroupInfo | undefined = localGroups.find(
+    (group) => group.id === formData.id
+  );
+
   return (
     <>
       <PageTitle title="Modifier un événement" />
+      {/* <AllTextInputs formData={formData} updateFormData={updateFormData} /> */}
     </>
   );
 };
