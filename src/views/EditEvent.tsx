6c661ac984f8d145f7ef40d129d4f9665f051e89
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-08 14:46:35 +0200

    Add basic rendering of all saved inputs

--- src/views/EditEvent.tsx
+++ src/views/EditEvent.tsx
@@ -1,22 +1,53 @@
 import AllTextInputs from "../components/AllTextInputs";
-import { getLocalGroups, setLocalGroups } from "../utils/manageLocalStorage";
+import { getLocalGroups } from "../utils/manageLocalStorage";
 import PageTitle from "../components/PageTitle";
-import { GroupInfo } from "../typeUtils";
+import { GroupInfo, EmptyForm } from "../typeUtils";
 import { useState } from "react";
+import SubmitEvent from "../components/SubmitEvent";
+import { useLocation } from "react-router-dom";
 
 const EditEvent = () => {
-  const [formData, setFormData] = useState<GroupInfo>({} as GroupInfo);
+  // get id from URL
+  const path = useLocation().pathname;
+  const id = path.split("/")[2];
 
   const localGroups = getLocalGroups();
-
   const currentGroup: GroupInfo | undefined = localGroups.find(
-    (group) => group.id === formData.id
+    (thisGroup) => thisGroup.id === id
   );
+
+  // update group state as fields are changed
+  const [group, setGroup] = useState<GroupInfo>(currentGroup as GroupInfo);
+  const updateFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGroup((prevGroup) => ({
+      ...prevGroup,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const emptyFormState: EmptyForm = {
+    associationName: "",
+    eventName: "",
+    eventType: "",
+    numberOfQuote: "",
+    category: "",
+    soldBy: "",
+    comments: "",
+    companyName: "",
+    contactName: "",
+    telephoneNumber: "",
+    email: "",
+  };
 
   return (
     <>
       <PageTitle title="Modifier un événement" />
-      {/* <AllTextInputs formData={formData} updateFormData={updateFormData} /> */}
+      <AllTextInputs formData={group} updateFormData={updateFormData} />
+      {/* <SubmitEvent
+        emptyFormState={emptyFormState}
+        formData={formData}
+        setFormData={setFormData}
+      /> */}
     </>
   );
 };
