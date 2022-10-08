c2940f96057321c4715f91e9bff041be4c5211b2
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-08 21:39:38 +0200

    Link the app logo to the landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import { AppBar, Toolbar } from "@mui/material";
 import MohoEventsLogo from "./MohoEventsLogo";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <AppBar sx={{ mb: 2 }} position="static">
       <Toolbar sx={{ pt: 0.2, pb: 0.6 }}>
-        <MohoEventsLogo color="white" height={45} />
+        <Link to="/">
+          <MohoEventsLogo color="white" height={45} />
+        </Link>
       </Toolbar>
     </AppBar>
   );
