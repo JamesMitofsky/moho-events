f04e83b205cecbeaf2bae4abd6a7ca84c59e124f
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-08 20:43:08 +0200

    Remove uncalled parameter

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -2,7 +2,7 @@ import { List } from "@mui/material";
 import EventTile from "./GroupTile";
 import { GroupStateObj } from "../typeUtils";
 
-const GroupList = ({ groups, setGroups }: GroupStateObj) => {
+const GroupList = ({ groups }: GroupStateObj) => {
   const renderedGroups = groups.map((group) => (
     <EventTile key={group.id} {...group} />
   ));
