b8eda88c158c0d4c10b336b246ee7ad802ef217d
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-09 15:23:00 +0200

    Switch to hook based solution for handling local storage and React state

--- src/views/CreateEvent.tsx
+++ src/views/CreateEvent.tsx
@@ -7,37 +7,14 @@ import AllTextInputs from "../components/AllTextInputs";
 import PageTitle from "../components/PageTitle";
 import { emptyFormState } from "../utils/globalVars";
 import ReturnHome from "../components/ReturnHome";
+import { useStorageState } from "react-storage-hooks";
 
 const NewEvent = () => {
-  // track form data
-  const [formData, setFormData] = useState<GroupInfo>({
-    ...emptyFormState,
-    id: uuidv4(),
-  });
-
-  // track state of local storage to avoid overwriting it
-  const [localChecked, setLocalChecked] = useState<boolean>(false);
-
-  // check for pre-existing data before making React state the point of truth
-  useEffect(() => {
-    const localData = localStorage.getItem(
-      import.meta.env.VITE_UNFINISHED_GROUP
-    );
-    if (localData) {
-      setFormData(JSON.parse(localData));
-    }
-    setLocalChecked(true);
-  }, []);
-
-  // when form data changes, push immediately to local storage
-  useEffect(() => {
-    if (!localChecked) return;
-
-    localStorage.setItem(
-      import.meta.env.VITE_UNFINISHED_GROUP,
-      JSON.stringify(formData)
-    );
-  }, [formData]);
+  const [formData, setFormData, writeError] = useStorageState<GroupInfo>(
+    localStorage,
+    import.meta.env.VITE_UNFINISHED_GROUP,
+    { ...emptyFormState, id: uuidv4() }
+  );
 
   const updateFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prevFormData) => ({
