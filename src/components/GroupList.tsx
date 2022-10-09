f7f6d2bf64e6370f276be73ad81cedd372a8a961
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-09 15:32:38 +0200

    Switch from MUI's Grid to Grid2

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,12 +1,12 @@
 import EventTile from "./GroupTile";
-import { GroupStateObj, GroupInfo } from "../utils/globalTypes";
-import Grid from "@mui/material/Grid"; // Grid version 2
+import { GroupStateObj } from "../utils/globalTypes";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const GroupList = ({ groups }: GroupStateObj) => {
   return (
     <Grid container spacing={{ lg: 6 }}>
       {groups.map((group) => (
-        <Grid key={group.id} item xs={12} lg={6}>
+        <Grid key={group.id} xs={12} lg={6}>
           <EventTile {...group} />
         </Grid>
       ))}
