9af3d6a501d7091dc3c7957f1137ddd2612b4a28
James Mitofsky <JamesMitofsky@gmail.com>
2022-10-09 15:31:02 +0200

    Simplify landing page list of groups & add key to each list item

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,17 +1,13 @@
 import EventTile from "./GroupTile";
-import { GroupStateObj } from "../utils/globalTypes";
+import { GroupStateObj, GroupInfo } from "../utils/globalTypes";
 import Grid from "@mui/material/Grid"; // Grid version 2
 
 const GroupList = ({ groups }: GroupStateObj) => {
-  const renderedGroups = groups.map((group) => (
-    <EventTile key={group.id} {...group} />
-  ));
-
   return (
     <Grid container spacing={{ lg: 6 }}>
-      {renderedGroups.map((group) => (
-        <Grid item xs={12} lg={6}>
-          {group}
+      {groups.map((group) => (
+        <Grid key={group.id} item xs={12} lg={6}>
+          <EventTile {...group} />
         </Grid>
       ))}
     </Grid>
