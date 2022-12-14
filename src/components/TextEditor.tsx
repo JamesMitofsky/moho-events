7dcde01884af3a0183d51ee0e749dd9303381bfb
James Mitofsky <JamesMitofsky@gmail.com>
2022-12-14 16:20:19 +0100

    Remove unused TextEditor component props

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -4,12 +4,11 @@ import { Controller } from "react-hook-form";
 import { Typography, Box } from "@mui/material";
 
 interface Props {
-  register: any;
   objLabel: string;
   control: any;
 }
 
-export default function TextEditor({ register, control, objLabel }: Props) {
+export default function TextEditor({ control, objLabel }: Props) {
   return (
     <Box sx={{ mb: 1, mt: 1 }}>
       <Controller
