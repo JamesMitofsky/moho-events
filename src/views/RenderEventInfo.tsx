c89e5f2bcdca7e46ce2a8005b2ca7f2976fc3b18
James Mitofsky <JamesMitofsky@gmail.com>
2023-01-03 14:55:10 +0100

    Add a basic display view for event indicated in the URL path

--- src/views/RenderEventInfo.tsx
+++ src/views/RenderEventInfo.tsx
@@ -1,26 +1,51 @@
-import { useParams, useLocation } from "react-router-dom";
-import { Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 import PageTitle from "../components/Layouts/PageTitle";
 import EventAvailableIcon from "@mui/icons-material/EventAvailable";
 import ReturnHome from "../components/ReturnHome";
+import { fetchSpecificEvent } from "../services/cloudFirestore";
+import { useEffect, useState } from "react";
+import { ModifiedServerResponse } from "../utils/globalTypes";
 
 export default function RenderEventInfo() {
-  const { eventID } = useParams<{ eventID: string }>();
-  const path = useLocation().pathname;
+  const { eventId } = useParams<{ eventId: string }>();
 
-  // use the ID from URL to query Firebase API
+  const [eventData, setEventData] = useState<ModifiedServerResponse>(
+    {} as ModifiedServerResponse
+  );
 
-  const pageTitle = "TITLE";
+  // query firebase suing the eventId
+  useEffect(() => {
+    const getEvent = async function () {
+      const res = await fetchSpecificEvent(eventId as string);
+      setEventData(res);
+    };
+    getEvent();
+  }, [eventId]);
 
   return (
     <>
       <ReturnHome />
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ display: "grid", gap: 2 }}>
         <PageTitle
-          title={pageTitle}
+          title={eventData?.society?.eventName}
           subtitle="événement"
           icon={EventAvailableIcon}
         />
+        {eventData?.society?.category && (
+          <Box sx={{ display: "grid", gap: 2 }}>
+            <Typography>
+              Association Name: {eventData.society.associationName}
+            </Typography>
+            <Typography>Category: {eventData.society.category}</Typography>
+            <Typography>Event Name: {eventData.society.eventName}</Typography>
+            <Typography>
+              Number of Quote: {eventData.society.numberOfQuote}
+            </Typography>
+            <Typography>Sold by: {eventData.society.soldBy}</Typography>
+            <Typography>Comments: {eventData.society.comments}</Typography>
+          </Box>
+        )}
       </Box>
     </>
   );
