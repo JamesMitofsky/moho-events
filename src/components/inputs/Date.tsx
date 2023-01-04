356b0b66b2ef26315de7e4651c1f211f29591818
James Mitofsky <JamesMitofsky@gmail.com>
2023-01-04 15:23:52 +0100

    Disable date component while displaying data & supress TypeScript errors with 'any'

--- src/components/inputs/Date.tsx
+++ src/components/inputs/Date.tsx
@@ -6,6 +6,9 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { fr } from "date-fns/locale";
 import { AllEventGroups } from "../../utils/globalTypes";
 import { Controller, Control, FieldPath } from "react-hook-form";
+import { fromUnixTime } from "date-fns";
+import IsReadOnly from "../../services/IsReadOnly";
+import { useContext } from "react";
 
 interface Props {
   control: Control<AllEventGroups>;
@@ -18,20 +21,37 @@ export default function Date({
   dataLabel,
   textLabel = "Date",
 }: Props) {
+  const isReadOnly = useContext<boolean>(IsReadOnly);
+  console.log(isReadOnly);
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={fr}>
       <Controller
         control={control}
         name={dataLabel}
         defaultValue={null}
-        render={({ field }) => (
-          <DatePicker
-            label={textLabel}
-            inputFormat="dd/MM/yyyy"
-            {...field}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        )}
+        render={({
+          field: { value, onChange },
+        }: {
+          field: { value: any; onChange: any };
+        }) => {
+          // when rendering the form for display only, the value arrives as an object with a seconds property. Change this into an object.
+          let formattedSecondsToDate = value;
+          const seconds: number = value?.seconds;
+          if (seconds) {
+            formattedSecondsToDate = fromUnixTime(seconds);
+          }
+
+          return (
+            <DatePicker
+              readOnly={isReadOnly}
+              label={textLabel}
+              inputFormat="dd/MM/yyyy"
+              value={formattedSecondsToDate}
+              onChange={(newValue) => onChange(newValue)}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          );
+        }}
       />
     </LocalizationProvider>
   );
