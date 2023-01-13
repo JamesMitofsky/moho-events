b01a5821cdbc139a12f2fd772ba22699c31310be
James Mitofsky <JamesMitofsky@gmail.com>
2023-01-13 15:49:45 +0100

    Add image to landing page & loading state to login button

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,10 +1,36 @@
+import PageTitle from "../components/Layouts/PageTitle";
+import CalendarPeople from "../components/design/CalendarPeople";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
 import { signInWithGoogle } from "../services/firebase";
-import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { useState } from "react";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    signInWithGoogle();
+  };
+
   return (
     <>
-      <Button onClick={signInWithGoogle}>login here</Button>
+      <PageTitle title="Bienvenu" />
+      <Typography variant="subtitle1">
+        Moho Events permet de facilement ajouter, modifier et partager les
+        evenements.
+      </Typography>
+      <LoadingButton
+        startIcon={<HowToRegIcon />}
+        variant="outlined"
+        onClick={handleClick}
+        loading={isLoading}
+        sx={{ mt: 5, mb: 5 }}
+      >
+        Authentifier avec Google
+      </LoadingButton>
+      <CalendarPeople widthLimit={3} />
     </>
   );
 }
