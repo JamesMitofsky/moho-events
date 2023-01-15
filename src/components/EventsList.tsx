ceeefe83b699570148f53f48d4940dfe6dd8873e
James Mitofsky <JamesMitofsky@gmail.com>
2023-01-15 14:50:34 +0100

    Seperate future and past events into their own lists

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -1,12 +1,13 @@
 import { fetchAllEvents } from "../services/cloudFirestore";
 import { useEffect, useState } from "react";
-import { ListItem, List, ListItemText } from "@mui/material";
+import { ListItem, List, ListItemText, Typography } from "@mui/material";
 import { ModifiedServerResponse } from "../utils/globalTypes";
 import { Link } from "react-router-dom";
 import filterDateOrNumberToDate from "../utils/filterDateOrNumberToDate";
 
 export default function EventsList() {
   const [events, setEvents] = useState<ModifiedServerResponse[]>([]);
+  const [pastEvents, setPastEvents] = useState<ModifiedServerResponse[]>([]);
 
   useEffect(() => {
     const getEvents = async function () {
@@ -28,45 +29,99 @@ export default function EventsList() {
         }
       });
 
-      setEvents(res);
+      // remove events which have no date or the date is passed
+      const upcomingEvents = res.filter((event) => {
+        const date = event.program?.eventDate as { seconds: number };
+        if (date) {
+          return date.seconds > Date.now() / 1000;
+        } else {
+          return false;
+        }
+      });
+      setEvents(upcomingEvents);
+
+      // array of events which have no date or the date is passed
+      const pastEvents = res.filter((event) => {
+        const date = event.program?.eventDate as { seconds: number };
+        if (date) {
+          return date.seconds < Date.now() / 1000;
+        } else {
+          return true;
+        }
+      });
+      setPastEvents(pastEvents);
     };
     getEvents();
   }, []);
 
   return (
-    <List>
-      {events.map((event) => {
-        const eventName = event.society?.eventName;
-        const associationName = event.society?.associationName;
+    <>
+      <Typography variant="subtitle2">Prochains événements</Typography>
+      <List>
+        {events.map((event) => {
+          const eventName = event.society?.eventName;
+          const associationName = event.society?.associationName;
 
-        const dateFromServer = event.program?.eventDate as {
-          seconds: number;
-        };
+          const dateFromServer = event.program?.eventDate as {
+            seconds: number;
+          };
 
-        const formattedDate = dateFromServer
-          ? filterDateOrNumberToDate(dateFromServer.seconds).toLocaleDateString(
-              "fr-FR",
-              {
+          const formattedDate = dateFromServer
+            ? filterDateOrNumberToDate(
+                dateFromServer.seconds
+              ).toLocaleDateString("fr-FR", {
                 weekday: "long",
                 month: "long",
                 day: "numeric",
-              }
-            )
-          : "Pas de date";
+              })
+            : "Pas de date";
 
-        // console.log(typeof dateFromServer, dateFromServer);
+          return (
+            <Link key={event.docId} to={`/evenement/${event.docId}`}>
+              <ListItem divider>
+                <ListItemText
+                  primary={`${eventName} - ${associationName}`}
+                  secondary={formattedDate}
+                />
+              </ListItem>
+            </Link>
+          );
+        })}
+      </List>
+      <Typography variant="subtitle2" sx={{ mt: 10 }}>
+        Événements passés
+      </Typography>
+      <List>
+        {pastEvents.map((event) => {
+          const eventName = event.society?.eventName;
+          const associationName = event.society?.associationName;
 
-        return (
-          <Link key={event.docId} to={`/evenement/${event.docId}`}>
-            <ListItem divider>
-              <ListItemText
-                primary={`${eventName} - ${associationName}`}
-                secondary={formattedDate}
-              />
-            </ListItem>
-          </Link>
-        );
-      })}
-    </List>
+          const dateFromServer = event.program?.eventDate as {
+            seconds: number;
+          };
+
+          const formattedDate = dateFromServer
+            ? filterDateOrNumberToDate(
+                dateFromServer.seconds
+              ).toLocaleDateString("fr-FR", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })
+            : "Pas de date";
+
+          return (
+            <Link key={event.docId} to={`/evenement/${event.docId}`}>
+              <ListItem divider>
+                <ListItemText
+                  primary={`${eventName} - ${associationName}`}
+                  secondary={formattedDate}
+                />
+              </ListItem>
+            </Link>
+          );
+        })}
+      </List>
+    </>
   );
 }
