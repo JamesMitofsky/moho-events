f39acad7d1dad536498bfa057b6049107b60e7c8
James Mitofsky <JamesMitofsky@gmail.com>
2023-04-10 15:11:38 +0200

    #11 Connect dynamic routing with slugs

---
+++ src/pages/evenement/[id].tsx
@@ -0,0 +1,21 @@
+import { fetchSpecificEvent } from "@/services/cloudFirestore";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function SpecificEventInformation() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  // query firebase using the eventId
+  useEffect(() => {
+    if (!id) return;
+    const getEvent = async function () {
+      const res = await fetchSpecificEvent(id);
+      // setEventData(res);
+      console.log(res);
+    };
+    getEvent();
+  }, [id]);
+
+  return <>specific event info here, thanks. and voila, an id: {id}</>;
+}
