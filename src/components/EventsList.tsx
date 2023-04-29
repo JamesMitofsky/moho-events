ea2b17efea880860dc99187359acff048df34225
James Mitofsky <JamesMitofsky@gmail.com>
2023-04-29 18:39:12 +0200

    Create a stronger seperation of interests for elements on the main events list page

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -1,56 +1,42 @@
 import useFetchEventsGroupedByDate from "@/hooks/useFetchEventsGroupedByDate";
-import { Link, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { ModifiedServerResponse } from "@/types/globalTypes";
+import { List, Typography } from "@mui/material";
+import { v4 as uuid4 } from "uuid";
+import LinkToEvent from "./LinkToEvent";
 
 export default function EventsList() {
   const events = useFetchEventsGroupedByDate();
 
   console.log(events);
 
-  return (
-    <>
-      {/* <SpacedChildren
-        flexDirection="row"
-        additionalStyles={{ alignItems: "center" }}
-      >
-        <Typography variant="subtitle1">Aujourd'hui </Typography>
-        <Typography variant="subtitle2">{new Date().toDateString()}</Typography>
-      </SpacedChildren> */}
-      {/* <List></List> */}
+  const eventsGroupedByDay = events?.map((eventGroup) => {
+    const trimmedDate = trimDateString(eventGroup[0]);
+    const constructedGroup = eventGroup.map((event) => {
+      return (
+        <LinkToEvent
+          associationName={event.generalInfo?.associationName}
+          docId={event.docId}
+          numberOfPeople={event.generalInfo?.numberOfPeople}
+        />
+      );
+    });
+    return (
+      <List key={uuid4()}>
+        <Typography>{trimmedDate}</Typography>
+        {constructedGroup}
+      </List>
+    );
+  });
 
-      {/* TODO: filter these to be in a chronology */}
-      {events?.map((eventGroup) => {
-        const trimDateString = () => {
-          const stringifiedDate = eventGroup[0].generalInfo.eventDate;
-          const dateObj = new Date(stringifiedDate);
-          const trimmedDate = dateObj.toLocaleString("fr", {
-            month: "short",
-            day: "numeric",
-          });
-          return trimmedDate;
-        };
+  return <>{eventsGroupedByDay}</>;
+}
 
-        const trimmedDate = trimDateString();
-        const constructedGroup = eventGroup.map((event) => {
-          const associationName = event.generalInfo?.associationName;
-
-          return (
-            <Link key={event.docId} href={`/evenement/${event.docId}`}>
-              <ListItem divider>
-                <ListItemText
-                  primary={`${associationName}`}
-                  secondary={"Event type information here"}
-                />
-              </ListItem>
-            </Link>
-          );
-        });
-        return (
-          <List>
-            <Typography>{trimmedDate}</Typography>
-            {constructedGroup}
-          </List>
-        );
-      })}
-    </>
-  );
+function trimDateString(eventGroup: ModifiedServerResponse) {
+  const stringifiedDate = eventGroup.generalInfo.eventDate;
+  const dateObj = new Date(stringifiedDate);
+  const trimmedDate = dateObj.toLocaleString("fr", {
+    month: "short",
+    day: "numeric",
+  });
+  return trimmedDate;
 }
---
+++ src/components/LinkToEvent.tsx
@@ -0,0 +1,25 @@
+import { Link, ListItem, ListItemText } from "@mui/material";
+import NextLink from "next/link";
+
+type Props = {
+  docId: string;
+  associationName: string;
+  numberOfPeople: string;
+};
+
+export default function LinkToEvent({
+  docId,
+  associationName,
+  numberOfPeople,
+}: Props) {
+  return (
+    <Link key={docId} href={`/evenement/${docId}`} component={NextLink}>
+      <ListItem divider>
+        <ListItemText
+          primary={`${associationName}`}
+          secondary={numberOfPeople && `${numberOfPeople} personnes`}
+        />
+      </ListItem>
+    </Link>
+  );
+}
