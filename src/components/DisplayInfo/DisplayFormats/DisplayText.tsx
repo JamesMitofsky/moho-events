38c65ff342b17c244f456a41a312ed1cbba1d8f8
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-01 15:45:51 +0200

    Add copy to clipboard feature for when viewing an event

--- src/components/DisplayInfo/DisplayFormats/DisplayText.tsx
+++ src/components/DisplayInfo/DisplayFormats/DisplayText.tsx
@@ -1,4 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Box, IconButton, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 interface Props {
   content: string;
@@ -6,12 +8,30 @@ interface Props {
 }
 
 export default function DisplayText({ content, label }: Props) {
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+  };
+
   // display simply a dash if the content is empty
   return (
     <Box>
-      <Typography sx={{ fontSize: 14, color: "grey" }}>{label}</Typography>
+      <Typography sx={{ fontSize: 14, color: grey }}>{label}</Typography>
       {content.length > 0 ? (
-        <Typography sx={{ fontSize: 18 }}>{content}</Typography>
+        <Box
+          sx={{
+            backgroundColor: grey[200],
+            py: 0.8,
+            px: 1,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontSize: 18 }}>{content}</Typography>
+          <IconButton onClick={() => copyToClipboard(content)}>
+            <ContentCopyIcon />
+          </IconButton>
+        </Box>
       ) : (
         <Typography sx={{ fontSize: 14 }}>—</Typography>
       )}
