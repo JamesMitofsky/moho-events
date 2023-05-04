d9fd477aa49e3a83c765159c10928e059dc1997d
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-04 17:11:38 +0200

    Merge into one of the last two commits

--- src/components/DisplayInfo/DisplayFormats/DisplayList.tsx
+++ src/components/DisplayInfo/DisplayFormats/DisplayList.tsx
@@ -1,4 +1,5 @@
 import { List, ListItem, Typography } from "@mui/material";
+import { v4 as uuid4 } from "uuid";
 import DisplayEmptyField from "./DisplayEmptyField";
 
 type Props = { items: string[]; label: string };
@@ -9,7 +10,7 @@ export default function DisplayList({ items, label }: Props) {
       <Typography>{label}</Typography>
       <List>
         {items.map((item) => {
-          return <ListItem>{item}</ListItem>;
+          return <ListItem key={uuid4()}>{item}</ListItem>;
         })}
       </List>
     </DisplayEmptyField>
