dc2900e6c5cb6f77f0266dafba2f71d1b88b9d76
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-05 16:20:02 +0200

    Add signage display component

--- src/components/DisplayInfo/DisplaySignage.tsx
+++ src/components/DisplayInfo/DisplaySignage.tsx
@@ -1,11 +1,42 @@
 import SignpostIcon from "@mui/icons-material/Signpost";
+import { Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import Grid from "@mui/system/Unstable_Grid";
+import { v4 as uuid4 } from "uuid";
 import { SignageInputs } from "../../types/globalTypes";
 import { TitledGroup } from "../layouts/TitledGroup";
+import TitledArrayOfElements from "../layouts/TitledItemFromArray";
 import DisplayHTML from "./DisplayFormats/DisplayHTML";
+import DisplayText from "./DisplayFormats/DisplayText";
 
-export default function DisplaySignage({ comments }: SignageInputs) {
+export default function DisplaySignage({
+  locationAndText,
+  comments,
+}: SignageInputs) {
   return (
     <TitledGroup icon={SignpostIcon} title="Signalétique">
+      {locationAndText[0].location !== "" ? (
+        locationAndText.map((item, index) => {
+          return (
+            <TitledArrayOfElements
+              key={uuid4()}
+              label="Signalétique"
+              index={index}
+              listLength={locationAndText.length}
+            >
+              <DisplayText content={item.location} label="Lieu" />
+              <DisplayText content={item.text} label="Contenu" />
+            </TitledArrayOfElements>
+          );
+        })
+      ) : (
+        <Grid xs={12}>
+          <Typography sx={{ color: grey[600], fontSize: 12 }}>
+            Aucune partie de signalétique
+          </Typography>
+        </Grid>
+      )}
+
       <DisplayHTML html={comments} label="Remarques" />
     </TitledGroup>
   );
