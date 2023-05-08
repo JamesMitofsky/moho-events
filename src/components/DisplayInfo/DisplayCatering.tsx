1ac625c8ceffb2f48a1ac68d341d7b7610edafa3
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-08 20:48:13 +0200

    Add conditional rendering and more complex mapping to the Display Catering page

--- src/components/DisplayInfo/DisplayCatering.tsx
+++ src/components/DisplayInfo/DisplayCatering.tsx
@@ -1,24 +1,43 @@
 import { ProgramInputs } from "@/types/globalTypes";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import { Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import Grid from "@mui/system/Unstable_Grid";
 import { v4 as uuid4 } from "uuid";
 import { TitledGroup } from "../layouts/TitledGroup";
+import TitledArrayOfElements from "../layouts/TitledItemFromArray";
 import CateringDetails from "./catering-details/CateringDetails";
 
 export default function DisplayCatering({
   events,
 }: Pick<ProgramInputs, "events">) {
   const caterings = events.flatMap((event) => {
-    if (event.involvesRestaurant) return [];
-    return [event];
+    if (event.involvesRestaurant) return [event];
+    return [];
   });
-
-  const allCateringGroups = caterings.map((catering) => (
-    <CateringDetails key={uuid4()} {...catering} />
-  ));
 
   return (
     <TitledGroup icon={RestaurantMenuIcon} title="Restauration">
-      {allCateringGroups}
+      {caterings[0]?.place ? (
+        caterings.map((catering, index) => {
+          return (
+            <TitledArrayOfElements
+              key={uuid4()}
+              label="Resturation"
+              index={index}
+              listLength={caterings.length}
+            >
+              <CateringDetails key={uuid4()} {...catering} />
+            </TitledArrayOfElements>
+          );
+        })
+      ) : (
+        <Grid xs={12}>
+          <Typography sx={{ color: grey[600], fontSize: 12 }}>
+            Acune partie de restauration
+          </Typography>
+        </Grid>
+      )}
     </TitledGroup>
   );
 }
