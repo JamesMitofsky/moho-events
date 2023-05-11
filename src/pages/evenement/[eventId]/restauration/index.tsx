cf6e959daee9f28bb106feaa66c6097a7d6f0364
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-11 17:08:37 +0200

    Use global theme to set a lighter h1 color

--- src/pages/evenement/[eventId]/restauration/index.tsx
+++ src/pages/evenement/[eventId]/restauration/index.tsx
@@ -1,16 +1,16 @@
 import DisplayCatering from "@/components/DisplayInfo/DisplayCatering";
-import DisplayGeneralInfo from "@/components/DisplayInfo/DisplayGeneralInfo";
 import Loading from "@/components/Loading";
 import useParamsToFetchEvent from "@/hooks/useParamsToFetchEvent";
+import { Typography } from "@mui/material";
 
 export default function CateringPage() {
   const event = useParamsToFetchEvent();
   return event ? (
     <>
-      <DisplayGeneralInfo
-        createdBy={event.creationDetails.createdBy}
-        {...event.generalInfo}
-      />
+      <Typography variant="h1">{event.generalInfo.associationName}</Typography>
+      <Typography sx={{ mb: 4 }} variant="subtitle1">
+        Evènement
+      </Typography>
       <DisplayCatering {...event.program} />
     </>
   ) : (
