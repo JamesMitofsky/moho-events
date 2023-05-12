86409b450a99bdf187b8e1afba1282451280c58b
James Mitofsky <JamesMitofsky@gmail.com>
2023-05-12 09:29:21 +0200

    Patch bug that can allow for bad processing of event end time

--- src/functions/getEventEndTime.ts
+++ src/functions/getEventEndTime.ts
@@ -1,7 +1,7 @@
 import { EventComponent } from "@/types/globalTypes";
 
 export default function getEventEndTime(events: EventComponent[]): string {
-  const lastEvent = events[events.length - 1];
+  const lastEvent = events.length > 1 ? events[events.length - 1] : events[0];
   const endTime = lastEvent.time.end as string;
   return endTime;
 }
