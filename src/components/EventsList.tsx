ac13335886167850c2cdc9d4f4dd29a9d65577f3
James Mitofsky <JamesMitofsky@gmail.com>
2023-06-06 16:58:30 +0200

    Bug fix: Add React `key` to elements in the EventsList component

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -1,7 +1,7 @@
 import groupEventsByDate from "@/functions/groupEventsByDate";
 import trimDateString from "@/functions/trimDateString";
 import useListenToAllEvents from "@/hooks/useListenToAllEvents";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import Grid from "@mui/system/Unstable_Grid";
 import { useMemo } from "react";
@@ -32,8 +32,8 @@ export default function EventsList({ eventsFilter, order = "asc" }: Props) {
       );
     });
     return (
-      <>
-        <Grid xs={12} key={uuid4()}>
+      <Box sx={{ width: "100%" }} key={uuid4()}>
+        <Grid xs={12}>
           <Typography sx={{ mt: 6, color: grey[700] }}>
             {trimmedDate}
           </Typography>
@@ -41,7 +41,7 @@ export default function EventsList({ eventsFilter, order = "asc" }: Props) {
         <Grid container spacing={2} xs={12}>
           {constructedGroup}
         </Grid>
-      </>
+      </Box>
     );
   });
 
